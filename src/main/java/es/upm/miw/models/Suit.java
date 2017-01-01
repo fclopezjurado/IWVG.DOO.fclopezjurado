13f730a161e02cba1bb8e58554ca2accf11d11dd
Francisco López <fclopezjurado@gmail.com>
2017-01-01 18:25:49 +0100

    Implementation of "Suit" Enumeration in "es.upm.miw.models" package

--- src/main/java/es/upm/miw/models/Suit.java
+++ src/main/java/es/upm/miw/models/Suit.java
@@ -8,5 +8,19 @@ package es.upm.miw.models;
  *
  */
 public enum Suit {
-
+	GOLDS("O"),
+	CUPS("C"),
+	SWORDS("E"),
+	CLUBS("B");
+	
+	private String suit;
+	
+	private Suit(String suit){
+		this.suit = suit;
+	}
+	
+	@Override
+	public String toString(){
+		return this.suit;
+	}
 }
