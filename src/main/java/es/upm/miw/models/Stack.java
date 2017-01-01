daeb070465401c241a599a256c87bb61478ca3cb
Francisco López <fclopezjurado@gmail.com>
2017-01-01 20:32:28 +0100

    Implementation of "Stack" class in "es.upm.miw.models" package

--- src/main/java/es/upm/miw/models/Stack.java
+++ src/main/java/es/upm/miw/models/Stack.java
@@ -3,9 +3,65 @@
  */
 package es.upm.miw.models;
 
+import java.util.ArrayList;
+
 /**
  * @author FCL
  *
  */
 public abstract class Stack {
+	private ArrayList<Card> cards;
+
+	/**
+	 * 
+	 */
+	protected Stack() {
+		this.cards = new ArrayList<Card>();
+	}
+
+	/**
+	 * @return
+	 */
+	protected int numberOfCards() {
+		return this.cards.size();
+	}
+
+	/**
+	 * @return
+	 */
+	protected int numberOfUpturnedCards() {
+		int numberOfUpturnedCards = 0;
+
+		for (Card card : this.cards)
+			if (card.isUpturned())
+				numberOfUpturnedCards++;
+
+		return numberOfUpturnedCards;
+	}
+
+	/**
+	 * @return
+	 */
+	protected Card pull() {
+		assert this.numberOfCards() > 0;
+
+		Card card = this.cards.get(this.numberOfCards() - 1);
+		this.cards.remove(card);
+
+		return card;
+	}
+
+	/**
+	 * @param card
+	 */
+	protected abstract void push(Card card);
+
+	/**
+	 * 
+	 */
+	protected void turnFirstCard() {
+		assert this.numberOfCards() > 0;
+
+		this.cards.get(this.numberOfCards() - 1).turn();
+	}
 }
