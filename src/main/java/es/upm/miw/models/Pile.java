1fe416a9af876199150dcea23a75e345df67061a
Francisco López <fclopezjurado@gmail.com>
2017-01-01 20:43:50 +0100

    Implementation of "Pile" class in "es.upm.miw.models" package

--- src/main/java/es/upm/miw/models/Pile.java
+++ src/main/java/es/upm/miw/models/Pile.java
@@ -7,5 +7,20 @@ package es.upm.miw.models;
  * @author FCL
  *
  */
-public class Pile {
+public class Pile extends Stack {
+	/**
+	 * 
+	 */
+	public Pile() {
+		super();
+	}
+
+	/*
+	 * @see es.upm.miw.models.Stack#push(es.upm.miw.models.Card)
+	 */
+	@Override
+	protected void push(Card card) {
+		assert card != null;
+		this.getCards().add(card);
+	}
 }
