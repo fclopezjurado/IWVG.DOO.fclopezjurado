cd642a3ebc1cd0552f20ae977857812d024d4604
Francisco López <fclopezjurado@gmail.com>
2017-01-01 18:39:36 +0100

    Implementation of "CardNumber" Enumeration in "es.upm.miw.models" package

--- src/main/java/es/upm/miw/models/CardNumber.java
+++ src/main/java/es/upm/miw/models/CardNumber.java
@@ -8,5 +8,22 @@ package es.upm.miw.models;
  *
  */
 public enum CardNumber {
+	ONE("1"), TWO("2"), THREE("3"), FOUR("4"), FIVE("5"), SIX("6"), SEVEN("7"), JACK("J"), HORSE("Q"), KING("K");
 
+	private String cardNumber;
+
+	/**
+	 * @param cardNumber
+	 */
+	private CardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public String toString() {
+		return this.cardNumber;
+	}
 }
