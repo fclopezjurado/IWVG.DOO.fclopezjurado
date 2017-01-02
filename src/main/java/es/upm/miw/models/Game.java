a724073eae280f9bcd6a9ccc2578f5f0b9b7d775
Francisco López <fclopezjurado@gmail.com>
2017-01-02 16:09:44 +0100

    Implementation of "Game" class in "es.upm.miw.models" package

--- src/main/java/es/upm/miw/models/Game.java
+++ src/main/java/es/upm/miw/models/Game.java
@@ -8,5 +8,36 @@ package es.upm.miw.models;
  *
  */
 public class Game {
+	private State state;
+	private Tableau tableau;
 
+	/**
+	 * 
+	 */
+	protected Game() {
+		this.state = State.INITIAL;
+		this.tableau = new Tableau();
+	}
+
+	/**
+	 * @return the state
+	 */
+	protected State getState() {
+		return state;
+	}
+
+	/**
+	 * @param state
+	 *            the state to set
+	 */
+	protected void setState(State state) {
+		this.state = state;
+	}
+
+	/**
+	 * @return the tableau
+	 */
+	protected Tableau getTableau() {
+		return tableau;
+	}
 }
