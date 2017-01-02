a79d20ca4861a9ba66280a992af26acab3bd25d4
Francisco López <fclopezjurado@gmail.com>
2017-01-02 20:07:32 +0100

    Implementation of "Klondike" class in "es.upm.miw.models" package

--- src/main/java/es/upm/miw/models/Klondike.java
+++ src/main/java/es/upm/miw/models/Klondike.java
@@ -3,18 +3,40 @@
  */
 package es.upm.miw.models;
 
+import es.upm.miw.controllers.Controller;
+
 /**
  * @author FCL
  *
  */
 public class Klondike {
+	private Logic logic;
+
+	/**
+	 * 
+	 */
+	protected Klondike() {
+		this.logic = new Logic();
+	}
+
+	/**
+	 * 
+	 */
+	private void play() {
+		Controller controller;
+
+		do {
+			controller = logic.getController();
+
+			if (controller != null)
+				controller.execute();
+		} while (controller != null);
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Klondike().play();
 	}
-
 }
