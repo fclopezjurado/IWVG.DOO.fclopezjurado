81bc04391be5f576a3a6ae040a101b6cc5fdc9d7
Francisco López <fclopezjurado@gmail.com>
2017-01-02 18:33:29 +0100

    Implementation of "Controller" abstract class in "es.upm.miw.controllers" package

--- src/main/java/es/upm/miw/controllers/Controller.java
+++ src/main/java/es/upm/miw/controllers/Controller.java
@@ -3,10 +3,29 @@
  */
 package es.upm.miw.controllers;
 
+import es.upm.miw.models.Game;
+
 /**
  * @author FCL
  *
  */
 public abstract class Controller {
+	private Game game;
 
+	/**
+	 * @param game
+	 */
+	protected Controller(Game game) {
+		assert game != null;
+		this.game = game;
+	}
+	
+	/**
+	 * @return the game
+	 */
+	protected Game getGame() {
+		return game;
+	}
+
+	protected abstract void execute();
 }
