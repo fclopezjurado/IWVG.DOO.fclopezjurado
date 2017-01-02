b84c065d6a8719cd83f73e0c94117f371a7e3ef9
Francisco López <fclopezjurado@gmail.com>
2017-01-02 20:02:25 +0100

    New "Klondike" class in "es.upm.miw.models" package. This class is defined to implement the main application class and is a controllers dispatcher. Depending on game state, this class launches the correct controller to execute an operation.

---
+++ src/main/java/es/upm/miw/models/Klondike.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package es.upm.miw.models;
+
+/**
+ * @author FCL
+ *
+ */
+public class Klondike {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
