ea888257a40298c2b68fb2bc27429846c648c462
Francisco López <fclopezjurado@gmail.com>
2017-01-02 11:40:12 +0100

    Implementation of "Option" enumeration in "es.upm.miw.models" package

--- src/main/java/es/upm/miw/models/Option.java
+++ src/main/java/es/upm/miw/models/Option.java
@@ -8,5 +8,29 @@ package es.upm.miw.models;
  *
  */
 public enum Option {
+	MOVE_FROM_DECK_TO_WASTE("Mover de baraja a descarte"), 
+	MOVE_FROM_WASTE_TO_DECK("Mover de descarte a baraja"), 
+	MOVE_FROM_WASTE_TO_FOUNDATION("Mover de descarte a palo"), 
+	MOVE_FROM_WASTE_TO_PILE("Mover de descarte a escalera"), 
+	MOVE_FROM_PILE_TO_FOUNDATION("Mover de escalera a palo"), 
+	MOVE_FROM_PILE_TO_PILE("Mover de escalera a escalera"), 
+	MOVE_FROM_FOUNDATION_TO_PILE("Mover de palo a escalera"), 
+	TURN_CARD_FROM_PILE("Voltear en escalera");
 
+	private String option;
+
+	/**
+	 * @param option
+	 */
+	private Option(String option) {
+		this.option = option;
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public String toString() {
+		return this.option;
+	}
 }
