fad8e69f3a4980ca96fe5f409e70a6b0b1cd6d05
Francisco López <fclopezjurado@gmail.com>
2017-01-02 18:19:45 +0100

    Implementation of "Error" enumeration in "es.upm.miw.utils" package

--- src/main/java/es/upm/miw/utils/Error.java
+++ src/main/java/es/upm/miw/utils/Error.java
@@ -8,5 +8,25 @@ package es.upm.miw.utils;
  *
  */
 public enum Error {
+	WRONG_MENU_OPTION("ERROR!!! La opción debe ser entre 1 y " + Option.values().length + " inclusives"),
+	INVALID_CARD_TURN("ERROR!!! No se puede voltear una carta descubierta"),
+	INVALID_PILE_NUMBER("ERROR!!! La escalera no existe"),
+	INVALID_SUIT("ERROR!!! El palo no existe");
+	
+	private String errorMessage;
 
+	/**
+	 * @param errorMessage
+	 */
+	private Error(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public String toString() {
+		return this.errorMessage;
+	}
 }
