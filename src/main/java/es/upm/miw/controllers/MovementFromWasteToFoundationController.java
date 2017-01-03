f847f960b34ce4ea6ee0393a6c5d23ac11042f00
Francisco López <fclopezjurado@gmail.com>
2017-01-03 20:21:48 +0100

    Implementation of "MovementFromWasteToFoundationController" class in "es.upm.miw.controllers" package.

--- src/main/java/es/upm/miw/controllers/MovementFromWasteToFoundationController.java
+++ src/main/java/es/upm/miw/controllers/MovementFromWasteToFoundationController.java
@@ -4,22 +4,38 @@
 package es.upm.miw.controllers;
 
 import es.upm.miw.models.Game;
+import es.upm.miw.models.State;
+import es.upm.miw.utils.IO;
+import es.upm.miw.utils.InputMessage;
+import es.upm.miw.utils.Error;
 
 /**
  * @author FCL
  *
  */
 public class MovementFromWasteToFoundationController extends Controller {
-
+	/**
+	 * 
+	 * @param game
+	 */
 	public MovementFromWasteToFoundationController(Game game) {
 		super(game);
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		
+		if (!this.getTableau().moveFromWasteToFoundation())
+			IO.getInstance().writeln(Error.INVALID_MOVEMENT_FROM_WASTE_TO_FOUNDATION.toString());
+
+		if (this.getTableau().areFoundationsFull()) {
+			this.setState(State.FINAL);
+			IO.getInstance().writeln(InputMessage.GAME_HAS_BEEN_FINISHED.toString());
+		}
+
+		this.getTableau().write();
 	}
 
 }
