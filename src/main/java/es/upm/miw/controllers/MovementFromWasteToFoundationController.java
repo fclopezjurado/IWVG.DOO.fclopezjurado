30793c8c141b652b3139fa45ea7dd14734b1ff6f
Francisco López <fclopezjurado@gmail.com>
2017-01-03 19:24:49 +0100

    New "MovementFromWasteToFoundationController" class in "es.upm.miw.controllers" package. This class is defined to implement "card movement from waste to foundation" use case.

---
+++ src/main/java/es/upm/miw/controllers/MovementFromWasteToFoundationController.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package es.upm.miw.controllers;
+
+import es.upm.miw.models.Game;
+
+/**
+ * @author FCL
+ *
+ */
+public class MovementFromWasteToFoundationController extends Controller {
+
+	public MovementFromWasteToFoundationController(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
