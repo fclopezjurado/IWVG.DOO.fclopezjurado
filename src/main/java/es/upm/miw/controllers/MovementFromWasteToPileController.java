c31060a9aca7f43e562182037643db5b7f8d2fe8
Francisco López <fclopezjurado@gmail.com>
2017-01-03 22:56:18 +0100

    Implementation of "MovementFromWasteToPileController" class in "es.upm.miw.controllers" package.

--- src/main/java/es/upm/miw/controllers/MovementFromWasteToPileController.java
+++ src/main/java/es/upm/miw/controllers/MovementFromWasteToPileController.java
@@ -4,22 +4,41 @@
 package es.upm.miw.controllers;
 
 import es.upm.miw.models.Game;
+import es.upm.miw.models.State;
+import es.upm.miw.models.Tableau;
+import es.upm.miw.utils.Error;
+import es.upm.miw.utils.IO;
+import es.upm.miw.utils.InputMessage;
 
 /**
  * @author FCL
  *
  */
 public class MovementFromWasteToPileController extends Controller {
-
+	/**
+	 * 
+	 * @param game
+	 */
 	public MovementFromWasteToPileController(Game game) {
 		super(game);
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
+		int pileNumber = IO.getInstance().readPileNumber(Tableau.PILES);
 		
+		if (!this.getTableau().moveFromWasteToPile(pileNumber))
+			IO.getInstance().writeln(Error.INVALID_MOVEMENT_FROM_WASTE_TO_PILE.toString());
+
+		if (this.getTableau().areFoundationsFull()) {
+			this.setState(State.FINAL);
+			IO.getInstance().writeln(InputMessage.GAME_HAS_BEEN_FINISHED.toString());
+		}
+
+		this.getTableau().write();
 	}
 
 }
