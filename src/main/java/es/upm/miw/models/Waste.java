6cc13e860135aaa4ec214918d0732aa837ced0ef
Francisco López <fclopezjurado@gmail.com>
2017-01-03 22:56:39 +0100

    Implementation of "Waste" class in "es.upm.miw.models" package.

--- src/main/java/es/upm/miw/models/Waste.java
+++ src/main/java/es/upm/miw/models/Waste.java
@@ -7,6 +7,39 @@ package es.upm.miw.models;
  * @author FCL
  *
  */
-public class Waste {
+public class Waste extends Stack {
+	protected static final int SIZE = 3;
+	
+	/**
+	 * 
+	 */
+	protected Waste() {
+		super();
+	}
 
+	/**
+	 * @see es.upm.miw.models.Stack#push(es.upm.miw.models.Card)
+	 */
+	@Override
+	protected void push(Card card) {
+		assert card != null;
+		this.getCards().add(card);
+	}
+
+	/**
+	 * 
+	 */
+	protected void upturnCards() {
+		for (Card card : this.getCards())
+			if (!card.isUpturned())
+				card.turn();
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	protected boolean isStackable(Card card) {
+		return true;
+	}
 }
