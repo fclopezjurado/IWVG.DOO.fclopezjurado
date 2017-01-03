7d1f08e21fb1d692a1da15db4b76af2b423631a6
Francisco López <fclopezjurado@gmail.com>
2017-01-03 17:34:49 +0100

    New "MovementFromWasteToDeckController" class in "es.upm.miw.controllers" package. This class is defined to implement "cards movements from waste to deck" use case.

---
+++ src/main/java/es/upm/miw/controllers/MovementFromWasteToDeckController.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package es.upm.miw.controllers;
+
+import es.upm.miw.models.Game;
+
+/**
+ * @author FCL
+ *
+ */
+public class MovementFromWasteToDeckController extends Controller {
+
+	public MovementFromWasteToDeckController(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
