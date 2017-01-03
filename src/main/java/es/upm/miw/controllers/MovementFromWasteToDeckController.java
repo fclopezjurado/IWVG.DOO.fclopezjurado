6a9449afe0862b95f03509acd39121e7ddefc845
Francisco López <fclopezjurado@gmail.com>
2017-01-03 17:37:35 +0100

    Implementation of "MovementFromWasteToDeckController" class in "es.upm.miw.controllers" package.

--- src/main/java/es/upm/miw/controllers/MovementFromWasteToDeckController.java
+++ src/main/java/es/upm/miw/controllers/MovementFromWasteToDeckController.java
@@ -4,22 +4,35 @@
 package es.upm.miw.controllers;
 
 import es.upm.miw.models.Game;
+import es.upm.miw.models.State;
+import es.upm.miw.utils.IO;
+import es.upm.miw.utils.InputMessage;
 
 /**
  * @author FCL
  *
  */
 public class MovementFromWasteToDeckController extends Controller {
-
+	/**
+	 * @param game
+	 */
 	public MovementFromWasteToDeckController(Game game) {
 		super(game);
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		
+		this.getTableau().moveToDeck();
+
+		if (this.getTableau().areFoundationsFull()) {
+			this.setState(State.FINAL);
+			IO.getInstance().writeln(InputMessage.GAME_HAS_BEEN_FINISHED.toString());
+		}
+
+		this.getTableau().write();
 	}
 
 }
