2d7417519b32505fef0c499d3e76d1751384abf2
Francisco López <fclopezjurado@gmail.com>
2017-01-03 22:56:58 +0100

    Implementation of "Foundation" class in "es.upm.miw.models" package.

--- src/main/java/es/upm/miw/models/Foundation.java
+++ src/main/java/es/upm/miw/models/Foundation.java
@@ -7,6 +7,30 @@ package es.upm.miw.models;
  * @author FCL
  *
  */
-public class Foundation {
+public class Foundation extends Stack {
+	/**
+	 * 
+	 */
+	protected Foundation() {
+		super();
+	}
 
+	/**
+	 * @see es.upm.miw.models.Stack#push(es.upm.miw.models.Card)
+	 */
+	@Override
+	protected void push(Card card) {
+		assert card != null;
+		this.getCards().add(card);
+	}
+
+	/**
+	 * @param card
+	 * @return
+	 */
+	protected boolean isStackable(Card card) {
+		return ((this.isEmpty() && (card.getNumber() == CardNumber.ONE)) || (!this.isEmpty() 
+				&& (this.getFirstCard().getPip() == card.getPip()) 
+				&& (this.getFirstCard().getNumber().ordinal() == (card.getNumber().ordinal() + 1))));
+	}
 }
