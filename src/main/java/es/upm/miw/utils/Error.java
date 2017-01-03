8e140dff8cd2434fc521895182a51235e97964b1
Francisco López <fclopezjurado@gmail.com>
2017-01-03 20:20:53 +0100

    Fixes in "Error" enumeration in "es.upm.miw.utils" package to add a new literal. This new literal will be used when there is an invalid movement from waste to foundation (INVALID_MOVEMENT_FROM_WASTE_TO_FOUNDATION)

--- src/main/java/es/upm/miw/utils/Error.java
+++ src/main/java/es/upm/miw/utils/Error.java
@@ -11,7 +11,8 @@ public enum Error {
 	WRONG_MENU_OPTION("ERROR!!! La opción debe ser entre 1 y " + Option.values().length + " inclusives"),
 	INVALID_CARD_TURN("ERROR!!! No se puede voltear una carta descubierta"),
 	INVALID_PILE_NUMBER("ERROR!!! La escalera no existe"),
-	INVALID_SUIT("ERROR!!! El palo no existe");
+	INVALID_SUIT("ERROR!!! El palo no existe"),
+	INVALID_MOVEMENT_FROM_WASTE_TO_FOUNDATION("ERROR!!! No se pudo mover de descarte a palo");
 	
 	private String errorMessage;
 
