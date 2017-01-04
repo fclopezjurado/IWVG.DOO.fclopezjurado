fdfd3e2244f67dcd14f79e2393bcd5b8ac3bfee1
Francisco López <fclopezjurado@gmail.com>
2017-01-04 16:55:45 +0100

    Implementation of "MovementFromPileToPileController" class in "es.upm.miw.controllers" package.

--- src/main/java/es/upm/miw/controllers/MovementFromPileToPileController.java
+++ src/main/java/es/upm/miw/controllers/MovementFromPileToPileController.java
@@ -4,22 +4,35 @@
 package es.upm.miw.controllers;
 
 import es.upm.miw.models.Game;
+import es.upm.miw.models.Tableau;
+import es.upm.miw.utils.Error;
+import es.upm.miw.utils.IO;
 
 /**
  * @author FCL
  *
  */
 public class MovementFromPileToPileController extends Controller {
-
+	/**
+	 * 
+	 * @param game
+	 */
 	public MovementFromPileToPileController(Game game) {
 		super(game);
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
+		int originPileNumber = IO.getInstance().readPileNumber(Tableau.PILES);
+		int numberOfCardsToMove = IO.getInstance().readNumberOfCards();
+		int destinyPileNumber = IO.getInstance().readPileNumber(Tableau.PILES);
 		
-	}
+		if (!this.getTableau().moveFromPileToPile(originPileNumber, destinyPileNumber, numberOfCardsToMove))
+			IO.getInstance().writeln(Error.INVALID_MOVEMENT_FROM_PILE_TO_PILE.toString());
 
+		this.getTableau().write();
+	}
 }
