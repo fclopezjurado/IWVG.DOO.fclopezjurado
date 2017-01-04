f9e03a97b195bb6c79864768d80497ecf7295417
Francisco López <fclopezjurado@gmail.com>
2017-01-04 13:58:01 +0100

    Implementation of "TurnController" class in "es.upm.miw.controllers" package.

--- src/main/java/es/upm/miw/controllers/TurnController.java
+++ src/main/java/es/upm/miw/controllers/TurnController.java
@@ -4,22 +4,32 @@
 package es.upm.miw.controllers;
 
 import es.upm.miw.models.Game;
+import es.upm.miw.models.Tableau;
+import es.upm.miw.utils.Error;
+import es.upm.miw.utils.IO;
 
 /**
  * @author FCL
  *
  */
 public class TurnController extends Controller {
-
+	/**
+	 * @param game
+	 */
 	public TurnController(Game game) {
 		super(game);
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
+		int pileNumber = IO.getInstance().readPileNumber(Tableau.PILES);
 		
-	}
+		if (!this.getTableau().turnCardInPile(pileNumber))
+			IO.getInstance().writeln(Error.INVALID_CARD_TURN.toString());
 
+		this.getTableau().write();
+	}
 }
