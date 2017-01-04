611dd3a43f7e30d900d4e4cded58c07d36fc46fb
Francisco López <fclopezjurado@gmail.com>
2017-01-04 13:43:23 +0100

    New "TurnController" class in "es.upm.miw.controllers" package. This class is defined to implement "turn card from pile" use case.

---
+++ src/main/java/es/upm/miw/controllers/TurnController.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package es.upm.miw.controllers;
+
+import es.upm.miw.models.Game;
+
+/**
+ * @author FCL
+ *
+ */
+public class TurnController extends Controller {
+
+	public TurnController(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
