8c1552c5c546c12f0e67192717540029f3bdb8e5
Francisco López <fclopezjurado@gmail.com>
2017-01-04 17:29:59 +0100

    Implementation of "MovementFromFoundationToPileController" class in "es.upm.miw.controllers" package.

--- src/main/java/es/upm/miw/controllers/MovementFromFoundationToPileController.java
+++ src/main/java/es/upm/miw/controllers/MovementFromFoundationToPileController.java
@@ -4,22 +4,35 @@
 package es.upm.miw.controllers;
 
 import es.upm.miw.models.Game;
+import es.upm.miw.models.Tableau;
+import es.upm.miw.utils.Error;
+import es.upm.miw.utils.IO;
 
 /**
  * @author FCL
  *
  */
 public class MovementFromFoundationToPileController extends Controller {
-
+	/**
+	 * 
+	 * @param game
+	 */
 	public MovementFromFoundationToPileController(Game game) {
 		super(game);
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
+		int foundationNumber = IO.getInstance().readFoundation(Tableau.FOUNDATIONS);
+		int destinyPileNumber = IO.getInstance().readPileNumber(Tableau.PILES);
 		
+		if (!this.getTableau().moveFromFoundationToPile(foundationNumber, destinyPileNumber))
+			IO.getInstance().writeln(Error.INVALID_MOVEMENT_FROM_FOUNDATION_TO_PILE.toString());
+
+		this.getTableau().write();
 	}
 
 }
