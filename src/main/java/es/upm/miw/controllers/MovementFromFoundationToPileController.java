679bb0dfac47c7554111b221f9ac4c05056e6451
Francisco López <fclopezjurado@gmail.com>
2017-01-04 16:58:46 +0100

    New "MovementFromFoundationToPileController" class in "es.upm.miw.controllers" package. This class is defined to implement "card movement from foundation to pile" use case.

---
+++ src/main/java/es/upm/miw/controllers/MovementFromFoundationToPileController.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package es.upm.miw.controllers;
+
+import es.upm.miw.models.Game;
+
+/**
+ * @author FCL
+ *
+ */
+public class MovementFromFoundationToPileController extends Controller {
+
+	public MovementFromFoundationToPileController(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
