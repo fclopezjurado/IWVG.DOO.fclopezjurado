7220e62bc8b95f1e75f49518a3674ab48206ea63
Francisco López <fclopezjurado@gmail.com>
2017-01-11 22:24:36 +0100

    Fixes in "Logic" class in "es.upm.miw.models" package to move to "es.upm.miw.controllers" package and adapt the methods.

--- src/main/java/es/upm/miw/controllers/Logic.java
+++ src/main/java/es/upm/miw/controllers/Logic.java
@@ -1,20 +1,11 @@
 /**
  * 
  */
-package es.upm.miw.models;
+package es.upm.miw.controllers;
 
 import java.util.ArrayList;
 
-import es.upm.miw.controllers.Controller;
-import es.upm.miw.controllers.MovementFromDeckToWasteController;
-import es.upm.miw.controllers.MovementFromFoundationToPileController;
-import es.upm.miw.controllers.MovementFromPileToFoundationController;
-import es.upm.miw.controllers.MovementFromPileToPileController;
-import es.upm.miw.controllers.MovementFromWasteToDeckController;
-import es.upm.miw.controllers.MovementFromWasteToFoundationController;
-import es.upm.miw.controllers.MovementFromWasteToPileController;
-import es.upm.miw.controllers.StartController;
-import es.upm.miw.controllers.TurnController;
+import es.upm.miw.models.Game;
 import es.upm.miw.utils.IO;
 import es.upm.miw.utils.InputMessage;
 
@@ -26,19 +17,13 @@ public class Logic {
 	private Game game;
 	private StartController startController;
 	private ArrayList<Controller> movementControllers;
-	
-	/**
-	 * 
-	 */
-	protected Logic() {
+
+	public Logic() {
 		this.game = new Game();
 		this.startController = new StartController(this.game);
 		this.initMovementControllers();
 	}
-	
-	/**
-	 * 
-	 */
+
 	private void initMovementControllers() {
 		this.movementControllers = new ArrayList<Controller>();
 		this.movementControllers.add(new MovementFromDeckToWasteController(this.game));
@@ -50,30 +35,24 @@ public class Logic {
 		this.movementControllers.add(new MovementFromFoundationToPileController(this.game));
 		this.movementControllers.add(new TurnController(this.game));
 	}
-	
-	/**
-	 * @return
-	 */
+
 	private Controller getMovementController() {
 		int movementControllerIndex = IO.getInstance().readMenuOption();
-		
+
 		if (movementControllerIndex > this.movementControllers.size()) {
 			IO.getInstance().writeln(InputMessage.EXIT.toString());
 			return null;
 		}
-		
+
 		return this.movementControllers.get(movementControllerIndex - 1);
 	}
-	
-	/**
-	 * @return
-	 */
+
 	public Controller getController() {
 		switch (game.getState()) {
 		case INITIAL:
 			return this.startController;
 		case IN_GAME:
-			return this.getMovementController();	
+			return this.getMovementController();
 		case FINAL:
 		default:
 			return null;
