b03323c9a59ebd33c8e335e5e5c5ac9d68092272
Francisco López <fclopezjurado@gmail.com>
2017-01-11 23:09:05 +0100

    Definition of "TableauView" class in "es.upm.miw.views" package.

--- src/main/java/es/upm/miw/views/TableauView.java
+++ src/main/java/es/upm/miw/views/TableauView.java
@@ -3,10 +3,23 @@
  */
 package es.upm.miw.views;
 
+import es.upm.miw.interfaces.PresenterController;
+
 /**
- * @author franlopez
+ * @author FCL
  *
  */
 public class TableauView {
+	
+	private PresenterController presenter;
+	
+	public TableauView(PresenterController presenter) {
+		assert presenter != null;
+		this.presenter = presenter;
+	}
+	
+	protected void write() {
+		
+	}
 
 }
