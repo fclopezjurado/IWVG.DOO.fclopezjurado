9a986043d516c583e93227848b0a281906d05695
Francisco López <fclopezjurado@gmail.com>
2017-01-11 20:14:18 +0100

    Definition of "PresenterController" interface in "es.upm.miw.interfaces" package.

--- src/main/java/es/upm/miw/interfaces/PresenterController.java
+++ src/main/java/es/upm/miw/interfaces/PresenterController.java
@@ -1,5 +1,28 @@
 package es.upm.miw.interfaces;
 
+import es.upm.miw.models.Card;
+import es.upm.miw.models.Pile;
+import es.upm.miw.models.Suit;
+import es.upm.miw.models.Waste;
+
 public interface PresenterController {
+
+	public boolean deckIsEmpty();
+
+	public boolean wasteIsEmpty();
+
+	public Waste getWaste();
+
+	public int numberOfFoundations();
+
+	public boolean foundationIsEmpty(Suit suit);
+
+	public Suit getFoundationSuit(int foundation);
+
+	public Card getFirstFoundationCard(int foundation);
+
+	public int numberOfPiles();
+
+	public Pile getPile(int pile);
 
 }
