6df84935ec4b5dc8b37c68a1d077f3b426a9dc44
Francisco López <fclopezjurado@gmail.com>
2017-01-11 22:24:11 +0100

    Fixes in "Klondike" class in "es.upm.miw.models" package to move to "es.upm.miw.main" pack and adapt the methods.

--- src/main/java/es/upm/miw/main/Klondike.java
+++ src/main/java/es/upm/miw/main/Klondike.java
@@ -1,9 +1,10 @@
 /**
  * 
  */
-package es.upm.miw.models;
+package es.upm.miw.main;
 
 import es.upm.miw.controllers.Controller;
+import es.upm.miw.controllers.Logic;
 
 /**
  * @author FCL
@@ -12,16 +13,10 @@ import es.upm.miw.controllers.Controller;
 public class Klondike {
 	private Logic logic;
 
-	/**
-	 * 
-	 */
 	protected Klondike() {
 		this.logic = new Logic();
 	}
 
-	/**
-	 * 
-	 */
 	private void play() {
 		Controller controller;
 
@@ -33,9 +28,6 @@ public class Klondike {
 		} while (controller != null);
 	}
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		new Klondike().play();
 	}
