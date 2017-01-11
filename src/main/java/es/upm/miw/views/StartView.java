782273ead280d775e96869adc01dad11883286c5
Francisco López <fclopezjurado@gmail.com>
2017-01-11 22:44:50 +0100

    Definition of "StartView" class in "es.upm.miw.views" package.

--- src/main/java/es/upm/miw/views/StartView.java
+++ src/main/java/es/upm/miw/views/StartView.java
@@ -3,10 +3,15 @@
  */
 package es.upm.miw.views;
 
+import es.upm.miw.interfaces.StartController;
+
 /**
  * @author FCL
  *
  */
 public class StartView {
+
+	public void interact(StartController startController) {
+	}
 
 }
