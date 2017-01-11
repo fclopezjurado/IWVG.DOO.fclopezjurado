412e520ad8292a25abc9ad99d410583290aeec71
Francisco López <fclopezjurado@gmail.com>
2017-01-11 20:05:15 +0100

    Definition of "ControllerVisitor" interface in "es.upm.miw.interfaces" package.

--- src/main/java/es/upm/miw/interfaces/ControllerVisitor.java
+++ src/main/java/es/upm/miw/interfaces/ControllerVisitor.java
@@ -2,4 +2,10 @@ package es.upm.miw.interfaces;
 
 public interface ControllerVisitor {
 
+	public void visit(StartController startController);
+
+	public void visit(MovementController movementController);
+
+	public void visit(MenuController menuController);
+
 }
