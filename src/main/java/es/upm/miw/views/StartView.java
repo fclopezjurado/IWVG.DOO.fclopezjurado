c5ce254265b2e16f93e6a9d922b8ba7175cf35bc
Francisco López <fclopezjurado@gmail.com>
2017-01-15 23:13:05 +0100

    Definition of "StartView" class in "es.upm.miw.views" package.

--- src/main/java/es/upm/miw/views/StartView.java
+++ src/main/java/es/upm/miw/views/StartView.java
@@ -1,17 +1,14 @@
-/**
- * 
- */
 package es.upm.miw.views;
 
 import es.upm.miw.interfaces.StartController;
 
-/**
- * @author FCL
- *
- */
 public class StartView {
 
 	protected void interact(StartController startController) {
+		startController.start();
+		TableauView tableau = new TableauView();
+		tableau.setPresenter(startController);
+		tableau.write();
 	}
 
 }
