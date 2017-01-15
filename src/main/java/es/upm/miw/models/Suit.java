30756910b170754a94977c7e6bfe16f217f859cc
Francisco López <fclopezjurado@gmail.com>
2017-01-15 23:10:54 +0100

    Definition of "Suit" enumeration in "es.upm.miw.models" package.

--- src/main/java/es/upm/miw/models/Suit.java
+++ src/main/java/es/upm/miw/models/Suit.java
@@ -1,29 +1,17 @@
-/**
- * 
- */
 package es.upm.miw.models;
 
-/**
- * @author FCL
- *
- */
 public enum Suit {
 	GOLDS("O"), CUPS("C"), SWORDS("E"), CLUBS("B");
 
 	private String suit;
 
-	/**
-	 * @param suit
-	 */
 	private Suit(String suit) {
 		this.suit = suit;
 	}
 
-	/*
-	 * @see java.lang.Enum#toString()
-	 */
 	@Override
 	public String toString() {
 		return this.suit;
 	}
+
 }
