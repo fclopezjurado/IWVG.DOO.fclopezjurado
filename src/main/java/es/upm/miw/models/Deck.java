f3b548b4c7f106f9c69abbf8367dd9d563072235
Francisco López <fclopezjurado@gmail.com>
2017-01-15 23:06:35 +0100

    Definition of "Deck" class in "es.upm.miw.models" package.

--- src/main/java/es/upm/miw/models/Deck.java
+++ src/main/java/es/upm/miw/models/Deck.java
@@ -1,39 +1,25 @@
-/**
- * 
- */
 package es.upm.miw.models;
 
-/**
- * @author FCL
- *
- */
 public class Deck extends Stack {
+
 	protected static final int SIZE = 40;
-		
-	/**
-	 * 
-	 */
+
 	protected Deck() {
 		super();
 	}
 
-	/**
-	 * 
-	 */
 	@Override
 	protected void push(Card card) {
 		this.getCards().add(0, card);
 	}
-	
-	/**
-	 * 
-	 */
+
 	protected boolean isStackable(Card card) {
 		if (this.numberOfCards() > 0)
 			for (Card cardInDeck : this.getCards())
 				if (cardInDeck.equals(card))
 					return false;
-		
+
 		return true;
 	}
+
 }
