38b49cfd70b171e92a711567fa8bce47074b17be
Francisco López <fclopezjurado@gmail.com>
2017-01-15 23:10:43 +0100

    Definition of "Pile" class in "es.upm.miw.models" package.

--- src/main/java/es/upm/miw/models/Pile.java
+++ src/main/java/es/upm/miw/models/Pile.java
@@ -1,57 +1,36 @@
-/**
- * 
- */
 package es.upm.miw.models;
 
-/**
- * @author FCL
- *
- */
 public class Pile extends Stack {
-	/**
-	 * 
-	 */
+
 	protected Pile() {
 		super();
 	}
 
-	/*
-	 * @see es.upm.miw.models.Stack#push(es.upm.miw.models.Card)
-	 */
 	@Override
 	protected void push(Card card) {
 		assert card != null;
 		this.getCards().add(card);
 	}
 
-	/**
-	 * @param card
-	 * @return
-	 */
 	protected boolean isStackable(Card card) {
-		return ((this.isEmpty() && (card.getNumber() == CardNumber.KING)) || (!this.isEmpty() 
-				&& (this.getFirstCard().getPip() != card.getPip()) 
-				&& (this.getFirstCard().getNumber().ordinal() == (card.getNumber().ordinal() + 1))));
+		return ((this.isEmpty() && (card.getNumber() == CardNumber.KING))
+				|| (!this.isEmpty() && (this.getFirstCard().getPip() != card.getPip())
+						&& (this.getFirstCard().getNumber().ordinal() == (card.getNumber().ordinal() + 1))));
 	}
-	
-	/**
-	 * 
-	 */
+
 	protected void turnFirstCard() {
 		if (!this.isEmpty())
 			this.getFirstCard().turn();
 	}
-	
-	/**
-	 * @return
-	 */
+
 	protected int numberOfUpturnedCards() {
 		int numberOfUpturnedCards = 0;
-		
+
 		for (Card card : this.getCards())
 			if (card.isUpturned())
 				numberOfUpturnedCards++;
-		
+
 		return numberOfUpturnedCards;
 	}
+
 }
