867aa3373ae1ac4d38de58d008c6653a2f254251
Francisco López <fclopezjurado@gmail.com>
2017-01-15 23:08:54 +0100

    Definition of "MovementFromDeckToWasteController" class in "es.upm.miw.controllers" package.

--- src/main/java/es/upm/miw/controllers/MovementFromDeckToWasteController.java
+++ src/main/java/es/upm/miw/controllers/MovementFromDeckToWasteController.java
@@ -1,28 +1,31 @@
-/**
- * 
- */
 package es.upm.miw.controllers;
 
+import es.upm.miw.interfaces.ControllerVisitor;
+import es.upm.miw.interfaces.MovementControllerVisitor;
+import es.upm.miw.interfaces.MovementToKnownDestinyController;
 import es.upm.miw.models.Game;
+import es.upm.miw.models.State;
 
-/**
- * @author FCL
- *
- */
-public class MovementFromDeckToWasteController extends Controller {
-	/**
-	 * @param game
-	 */
+public class MovementFromDeckToWasteController extends PresenterController implements MovementToKnownDestinyController {
+
 	public MovementFromDeckToWasteController(Game game) {
 		super(game);
 	}
 
-	/**
-	 * 
-	 */
 	@Override
-	public void execute() {
-		this.getTableau().moveToWaste();
-		this.getTableau().write();
+	public void accept(ControllerVisitor controllerVisitor) {
+		controllerVisitor.visit(this);
 	}
+
+	@Override
+	public boolean move() {
+		this.setState(State.GET_OPTION);
+		return this.getGame().moveToWaste();
+	}
+
+	@Override
+	public void accept(MovementControllerVisitor movementControllerVisitor) {
+		movementControllerVisitor.visit(this);
+	}
+
 }
