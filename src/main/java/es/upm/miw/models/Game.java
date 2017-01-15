9e9c23376c1d6256879b07c77145c593f4f6b857
Francisco López <fclopezjurado@gmail.com>
2017-01-15 23:07:04 +0100

    Definition of "Game" class in "es.upm.miw.models" package.

--- src/main/java/es/upm/miw/models/Game.java
+++ src/main/java/es/upm/miw/models/Game.java
@@ -1,19 +1,11 @@
-/**
- * 
- */
 package es.upm.miw.models;
 
-/**
- * @author FCL
- *
- */
-public class Game {
+public class Game extends TableauManager {
 	private State state;
-	private Tableau tableau;
 
 	public Game() {
+		super();
 		this.state = State.INITIAL;
-		this.tableau = new Tableau();
 	}
 
 	public State getState() {
@@ -24,7 +16,102 @@ public class Game {
 		this.state = state;
 	}
 
-	public Tableau getTableau() {
-		return tableau;
+	public boolean areFoundationsFull() {
+		for (Suit suit : Suit.values())
+			if (this.tableau.getFoundation(suit).numberOfCards() < CardNumber.values().length)
+				return false;
+
+		return true;
 	}
+
+	public boolean moveToWaste() {
+		this.moveToDeck();
+		boolean successful = this.tableau.moveCards(this.tableau.getDeck(), this.tableau.getWaste(), Waste.SIZE, true);
+
+		if (successful)
+			this.tableau.getWaste().upturnCards();
+
+		return successful;
+	}
+
+	public boolean moveToDeck() {
+		return this.tableau.moveCards(this.tableau.getWaste(), this.tableau.getDeck(),
+				this.tableau.getWaste().numberOfCards(), true);
+	}
+
+	public boolean moveFromWasteToFoundation() {
+		if (!this.tableau.getWaste().isEmpty())
+			return this.tableau.moveCards(this.tableau.getWaste(),
+					this.tableau.getFoundation(this.tableau.getWaste().getFirstCard().getPip()), 1, true);
+
+		return false;
+	}
+
+	public boolean moveFromWasteToPile(int pile) {
+		assert ((pile > 0) && (pile <= Tableau.PILES));
+
+		if (!this.tableau.getWaste().isEmpty())
+			return this.tableau.moveCards(this.tableau.getWaste(), this.tableau.getPile(pile - 1), 1, true);
+
+		return false;
+	}
+
+	public boolean moveFromPileToFoundation(int pile) {
+		assert ((pile > 0) && (pile <= Tableau.PILES));
+
+		Pile involvedPile = this.tableau.getPile(pile - 1);
+
+		if (!involvedPile.isEmpty())
+			return this.tableau.moveCards(involvedPile,
+					this.tableau.getFoundation(involvedPile.getFirstCard().getPip()), 1, true);
+
+		return false;
+	}
+
+	public boolean moveFromPileToPile(int origin, int destiny, int cards) {
+		assert ((origin > 0) && (origin <= Tableau.PILES));
+		assert ((destiny > 0) && (destiny <= Tableau.PILES));
+		assert cards > 0;
+
+		Pile originPile = this.tableau.getPile(origin - 1);
+		Pile destinyPile = this.tableau.getPile(destiny - 1);
+		Pile auxiliarPile = new Pile();
+
+		if (originPile.numberOfUpturnedCards() >= cards) {
+			this.tableau.moveCards(originPile, auxiliarPile, cards, false);
+			boolean successful = this.tableau.moveCards(auxiliarPile, destinyPile, cards, true);
+
+			if (successful)
+				return true;
+			else
+				this.tableau.moveCards(auxiliarPile, originPile, cards, false);
+		}
+
+		return false;
+	}
+
+	public boolean moveFromFoundationToPile(int foundation, int destiny) {
+		assert ((foundation > 0) && (foundation <= this.numberOfFoundations()));
+		assert ((destiny > 0) && (destiny <= this.numberOfPiles()));
+
+		Foundation involvedFoundation = this.tableau.getFoundation(Suit.values()[foundation - 1]);
+
+		if (!involvedFoundation.isEmpty())
+			return this.tableau.moveCards(involvedFoundation, this.tableau.getPile(destiny - 1), 1, true);
+
+		return false;
+	}
+
+	public boolean turnCard(int pile) {
+		assert ((pile > 0) && (pile <= Tableau.PILES));
+
+		Pile involvedPile = this.tableau.getPile(pile - 1);
+
+		if (involvedPile.getFirstCard().isUpturned())
+			return false;
+
+		involvedPile.turnFirstCard();
+		return true;
+	}
+
 }
