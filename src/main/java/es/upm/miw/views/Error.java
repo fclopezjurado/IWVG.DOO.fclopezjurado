7250393d42e41986a1ac6be7862b32863dc22415
Francisco López <fclopezjurado@gmail.com>
2017-01-15 23:06:50 +0100

    Definition of "Error" enumeration in "es.upm.miw.views" package.

--- src/main/java/es/upm/miw/views/Error.java
+++ src/main/java/es/upm/miw/views/Error.java
@@ -1,36 +1,20 @@
-/**
- * 
- */
-package es.upm.miw.utils;
+package es.upm.miw.views;
 
-/**
- * @author FCL
- *
- */
+import es.upm.miw.utils.Option;
+
 public enum Error {
-	WRONG_MENU_OPTION("ERROR!!! La opción debe ser entre 1 y " + Option.values().length + " inclusives"),
-	INVALID_CARD_TURN("ERROR!!! No se pudo voltear la carta"),
-	INVALID_PILE_NUMBER("ERROR!!! La escalera no existe"),
+	WRONG_MENU_OPTION("ERROR!!! La opción debe ser entre 1 y " + Option.values().length + " inclusives"), 
+	INVALID_MOVEMENT("ERROR!!! No se pudo realizar el movimiento"), 
+	INVALID_PILE_NUMBER("ERROR!!! La escalera no existe"), 
 	INVALID_SUIT("ERROR!!! El palo no existe"),
-	INVALID_MOVEMENT_FROM_WASTE_TO_FOUNDATION("ERROR!!! No se pudo mover de descarte a palo"),
-	INVALID_MOVEMENT_FROM_WASTE_TO_PILE("ERROR!!! No se pudo mover de descarte a escalera"),
-	INVALID_MOVEMENT_FROM_PILE_TO_FOUNDATION("ERROR!!! No se pudo mover de escalera a palo"),
-	INVALID_NUMBER_OF_CARD_TO_MOVE("ERROR!!! Se debe mover al menos una carta"),
-	INVALID_MOVEMENT_FROM_PILE_TO_PILE("ERROR!!! No se pudo mover de escalera a escalera"),
-	INVALID_MOVEMENT_FROM_FOUNDATION_TO_PILE("ERROR!!! No se pudo mover de palo a escalera");
-	
+	INVALID_NUMBER_OF_CARD_TO_MOVE("ERROR!!! Se debe mover al menos una carta");
+
 	private String errorMessage;
 
-	/**
-	 * @param errorMessage
-	 */
 	private Error(String errorMessage) {
 		this.errorMessage = errorMessage;
 	}
 
-	/**
-	 * 
-	 */
 	@Override
 	public String toString() {
 		return this.errorMessage;
