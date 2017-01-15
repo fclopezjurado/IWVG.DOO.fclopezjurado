20fd8422a37f7e594452f4e71e49dc79fc277e3b
Francisco López <fclopezjurado@gmail.com>
2017-01-15 23:07:49 +0100

    Definition of "Klondike" class in "es.upm.miw.main" package.

--- src/main/java/es/upm/miw/main/Klondike.java
+++ src/main/java/es/upm/miw/main/Klondike.java
@@ -1,20 +1,16 @@
-/**
- * 
- */
 package es.upm.miw.main;
 
 import es.upm.miw.controllers.Controller;
 import es.upm.miw.controllers.Logic;
+import es.upm.miw.views.KlondikeView;
 
-/**
- * @author FCL
- *
- */
 public class Klondike {
 	private Logic logic;
+	private KlondikeView klondikeView;
 
 	protected Klondike() {
 		this.logic = new Logic();
+		this.klondikeView = new KlondikeView();
 	}
 
 	private void play() {
@@ -24,11 +20,12 @@ public class Klondike {
 			controller = logic.getController();
 
 			if (controller != null)
-				controller.execute();
+				klondikeView.interact(controller);
 		} while (controller != null);
 	}
 
 	public static void main(String[] args) {
 		new Klondike().play();
 	}
+
 }
